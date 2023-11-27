4c78283301f9453b08b9f575b17124cdcd22c6c0
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-27 08:49:40 +0700

    add: image default icon on navbar

--- src/components/Navbar/NavbarKuyliner.jsx
+++ src/components/Navbar/NavbarKuyliner.jsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import LogoKuyliner from "../../assets/logo_kuyliner_2.png";
+import IconDefault from "../../assets/icon-default.png";
 
 function NavbarKuyliner() {
   return (
@@ -10,7 +11,10 @@ function NavbarKuyliner() {
         <Navbar.Brand className="d-flex">
           <img src={LogoKuyliner} style={{ height: "45px" }} />
         </Navbar.Brand>
-        <Navbar.Brand href="#">Account</Navbar.Brand>
+        <Navbar.Brand href="#" className="d-flex gap-3 align-items-center ">
+          <img src={IconDefault} style={{width: 40}} alt="" />
+          <h4 className="m-0 ">Account</h4>
+        </Navbar.Brand>
       </Container>
     </Navbar>
   );
